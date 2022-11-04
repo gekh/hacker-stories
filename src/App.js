df58c9753d30cf49a94236597a5344920eb2bc66
German Khokhlov <lkg0dzre@gmail.com>
2022-11-04 17:11:32 +0530

    Meet another React Component

--- src/App.js
+++ src/App.js
@@ -22,24 +22,41 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(item => 
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        )}
-        
-      </ul>
+      <List />
+
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(item =>
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          {' '}—{' '}
+          <span>{item.author}</span>
+          {' '}—{' '}
+          <span>{item.num_comments}</span>
+          {' '}—{' '}
+          <span>{item.points}</span>
+        </li>
+      )}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
