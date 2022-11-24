57d2333fb31c023757bd507bcb995c3f6c11c9fc
German Khokhlov <lkg0dzre@gmail.com>
2022-11-24 13:39:55 +0530

    Testing in React

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('removes an item when clicking the Dismiss button', () => {
+
+  });
+
+  test('requests some initial stories from an API', () => {
+
+  });
 });
+
+
+describe('something truthy and falsy', () => {
+  test('true to be true', () => {
+    expect(true).toBeTruthy;
+  });
+
+  test('false to be false', () => {
+    expect(false).toBeFalsy;
+  });
+});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
